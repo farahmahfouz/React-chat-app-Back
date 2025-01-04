4c9309864c2f52ce6034e0587eaddcbca9177810
Farah Mahfouz <122357697+farahmahfouz@users.noreply.github.com>
2025-01-05 00:49:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const channelRouter = require("./Routes/channelRoute.js");
 
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: 'https://react-chat-app-ten-flame.vercel.app',
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
